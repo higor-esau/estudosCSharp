fde8ec250e76e78e0724264a82ea44c5d7876b8d
Esau Higor <esauhigor@gmail.com>
2026-02-02 10:20:33 -0400

    Criando a função TryExecuteSale

--- store/src/saleservice.cs
+++ store/src/saleservice.cs
@@ -1,13 +1,31 @@
 class SaleService
 {
-    public bool Sale(
+    public bool TryExecuteSale(
         ICanSellItems seller, 
         ICanBuyItems buyer,
-        int idItem,
-        int quantity
-        )
+        int itemId,
+        int quantity)
     {
+        if (!seller.HasItemQuantity(itemId, quantity))
+            return false;
 
+        decimal total = seller.CalculateTotalPrice(itemId, quantity);
+
+        if (!buyer.HasMoney(total))
+            return false;
+
+        if (!seller.TryGetItem(itemId, out Item item))
+            return false;
+
+        //nesse codigo eu estou assumindo que o removeitem e SpendMoney nunca daram erro, vou adicionar uma exceção se isso vier a ocorrer, como o foco é treinar interfaces, não estou me atendando a tanto detalhes, mas não quer dizer que eu não o perceba.
+
+        // buyer
+        buyer.SpendMoney(total);
+        buyer.AddItem(item, quantity);
+
+        // seller
+        seller.ReceiveMoney(total);
+        seller.RemoveItem(itemId, quantity);
 
         return true;
     }
