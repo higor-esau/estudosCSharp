70c2d6cd596c614291a7b8e7f9cf2ab8c42a02b6
Esau Higor <esauhigor@gmail.com>
2026-02-02 07:41:18 -0400

    criando inventario e item do inventario

--- store/src/inventory.cs
+++ store/src/inventory.cs
@@ -1,9 +1,31 @@
+class InventoryItem
+{
+    private Item _item;
+    public int Quantity {get; private set;}
+
+    public int Id => _item.Id;
+    public bool HasQuantity(int qtd)
+    {
+        return qtd <= Quantity;
+    }
+}
+
 class Inventory
 {
-    private List<Item> items;
+    private List<InventoryItem> items;
 
-    public Inventory(List<Item> items)
+    public Inventory(List<InventoryItem> items)
     {
         this.items = items;
+    }
+
+    public bool HasItemQuantity(int itemId, int quantity)
+    {
+        foreach(var item in items)
+        {
+            if(itemId == item.Id)
+                return item.HasQuantity(quantity);
+        }
+        return false;
     }
 }
