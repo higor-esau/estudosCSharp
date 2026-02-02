a70eac66a2d668914cab6e7997c41b641c6c747e
Esau Higor <esauhigor@gmail.com>
2026-02-02 08:40:21 -0400

    retirando o reserv.cs

--- store/obj/Debug/net10.0/store.AssemblyInfo.cs
+++ store/obj/Debug/net10.0/store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a2bff1ce6b861dd0716cefe0fe9c16cbd45100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c7b3f1b27ae29dea32654040d63f5dfa8a8b7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
