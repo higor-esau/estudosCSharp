18eb26cb48a1a6e273ac1ae5c78a112def119a9c
Esau Higor <esauhigor@gmail.com>
2026-02-03 15:11:28 -0400

    Primeiro cenario de teste, esse está funcional, irei melhoralo no funturo

--- store/Program.cs
+++ store/Program.cs
@@ -2,6 +2,18 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Higor");
+        SaleService saleService = new SaleService();
+
+        //seller
+        Store store = new Store(1000);
+        decimal x = 1000;
+        //buyer
+        Player player = new Player("Higor",x);
+        player.Apresentation();
+        saleService.TryExecuteSale(store,player, 0, 2);
+        player.Apresentation();
+        saleService.TryExecuteSale(store,player, 0, 2);
+        player.Apresentation();
+
     }
 }
