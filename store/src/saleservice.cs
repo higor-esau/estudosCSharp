f69c83f873563bc1f8e1286eeb075d90b11df6fa
Esau Higor <esauhigor@gmail.com>
2026-02-03 19:26:14 -0400

    Criando alguns debugs e adicionando uma mensagem pra compras bem sucedidas

--- store/src/saleservice.cs
+++ store/src/saleservice.cs
@@ -9,15 +9,24 @@ class SaleService
         Console.WriteLine("#####INICIANDO UMA VENDA#####");
         
         if (!seller.HasItemQuantity(itemId, quantity))
+        {
+            Console.WriteLine("Não tenho a quantidade de item disponivel!");
             return false;
+        }
 
         decimal total = seller.CalculateTotalPrice(itemId, quantity);
 
         if (!buyer.HasMoney(total))
+        {
+            Console.WriteLine("O Comprador está sem money!");
             return false;
+        }
 
         if (!seller.TryGetItem(itemId, out Item? item))
+        {
+            Console.WriteLine("Erro estranho, impossivel pegar o item");
             return false;
+        }
 
         //nesse codigo eu estou assumindo que o removeitem e SpendMoney nunca daram erro, vou adicionar uma exceção se isso vier a ocorrer, como o foco é treinar interfaces, não estou me atendando a tanto detalhes, mas não quer dizer que eu não o perceba.
         if (item == null)
@@ -29,6 +38,10 @@ class SaleService
         // seller
         seller.ReceiveMoney(total);
         seller.RemoveItem(itemId, quantity);
+
+
+        //isso aqui deveria ser uma função aparte que cuidava se tudo desse certo, assim como eu precisava de uma pra se desse errado mais aqui é só estudos de interfaces
+        Console.WriteLine($"Você comprou {quantity} unidade(s) do item {item.Name}");
 
         return true;
     }
